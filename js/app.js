d4ba7402aff216653d994c978f366355b2155b59
Breno Silva <brenosilva1@gmail.com>
2018-03-21 22:30:55 -0300

    add ES6 alternative functions for jquery

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 //debounce for otimization
-
 debounce = function(func, wait, immediate) {
 	var timeout;
 	return function() {
@@ -15,26 +14,24 @@ debounce = function(func, wait, immediate) {
 	};
 };
 
-
-
 // Change tab
 $('[data-group]').each(function(){
 	var $allTarget = $(this).find('[data-target]'),
 			$allClick = $(this).find('[data-click]'),
 			activeClass = 'active';
-	
+
 	$allTarget.first().addClass(activeClass);
 	$allClick.first().addClass(activeClass);
-	
+
 	$allClick.click(function(e){
 		e.preventDefault();
-		
+
 		var id = $(this).data('click'),
 				$target = $('[data-target="' + id + '"]');
-		
+
 		$allClick.removeClass(activeClass);
 		$allTarget.removeClass(activeClass);
-		
+
 		$target.addClass(activeClass);
 		$(this).addClass(activeClass);
 	});
@@ -74,8 +71,7 @@ $('[data-group]').each(function(){
 // 	})
 // });
 
-
-//Smooth scroll to internal link
+////Smooth scroll to internal link
 $('.menu-nav a[href^="#"]').click(function(e){
 	e.preventDefault();
 	var id = $(this).attr('href'),
@@ -94,14 +90,14 @@ $('.logo').click(function(e){
 	}, 500)
 });
 
-//mChanging active state according to the section
+//Changing active state according to the section
 $('section').each(function(){
 	var height = $(this).height(),
 			offsetTop = $(this).offset().top,
 			menuHeight = $('.menu').innerHeight(),
 			id = $(this).attr('id'),
 			$itemMenu = $('a[href="#' + id + '"]');
-	
+
 	$(window).scroll(debounce(function(){
 		var scrollTop = $(window).scrollTop();
 		if(offsetTop - menuHeight < scrollTop && offsetTop + height - menuHeight > scrollTop) {
@@ -109,7 +105,7 @@ $('section').each(function(){
 		} else {
 			$itemMenu.removeClass('active');
 		}
-	}, 100));
+	}, 200));
 });
 
 //Mobile button
@@ -118,103 +114,99 @@ $('.mobile-btn').click(function(){
 	$('.mobile-menu').toggleClass('active');
 });
 
-
-$('.slide > :first').addClass('active');
-
-//slider
+ //Slider 
 (function(){
-	function slider(sliderName, velocidade){
+	function slider(sliderName, velocidade) {
 		var sliderClass = '.' + sliderName,
-		activeClass = 'active',
-		rotate = setInterval(rotateSlide, velocidade);
+				activeClass = 'active',
+				rotate = setInterval(rotateSlide, velocidade);
 
-
-		$(sliderClass +' > :first').addClass(activeClass);
+		$(sliderClass + ' > :first').addClass(activeClass);
 
 		$(sliderClass).hover(function(){
 			clearInterval(rotate);
-		}, function(){
+		}, function() {
 			rotate = setInterval(rotateSlide, velocidade);
-		})
+		});
 
 		function rotateSlide() {
 			var activeSlide = $(sliderClass + ' > .' + activeClass),
-				nextSlide = activeSlide.next();
+					nextSlide = activeSlide.next();
 
-			if (nextSlide.length == 0){
+			if(nextSlide.length == 0) {
 				nextSlide = $(sliderClass + ' > :first');
 			}
-
 			activeSlide.removeClass(activeClass);
 			nextSlide.addClass(activeClass);
-			return 0;
 		}
-	}
+}
 
-
-	slider('introducao', 2000);
+slider('introducao', 2000);
 })();
 
-
-// // ES6  - Slider
+// //ES6  - Slider
 // (function(){
-// function slider(sliderName, velocidade) {
-// 	var sliderClass = '.' + sliderName,
+// 	function slider(sliderName, speed){
+// 		let sliderClass = `.${sliderName}`,
 // 			activeClass = 'active',
-// 			rotate = setInterval(rotateSlide, velocidade);
+// 			rotate = setInterval(rotateSlide, speed);
 
-// 	$(sliderClass + ' > :first').addClass(activeClass);
+// 		let sliderFirst = document.querySelector(`${sliderClass} > div`);
+// 		sliderFirst.classList.add(activeClass);
 
-// 	$(sliderClass).hover(function(){
-// 		clearInterval(rotate);
-// 	}, function() {
-// 		rotate = setInterval(rotateSlide, velocidade);
-// 	});
+// 		let slider = document.querySelector(sliderClass);
 
-// 	function rotateSlide() {
-// 		var activeSlide = $(sliderClass + ' > .' + activeClass),
-// 				nextSlide = activeSlide.next();
+// 		slider.addEventListener('mouseover', () => {
+// 			clearInterval(rotate);	
+// 		}, () => {
+// 			rotate = setInterval(rotateSlide, speed);
+// 		});
 
-// 		if(nextSlide.length == 0) {
-// 			nextSlide = $(sliderClass + ' > :first');
+// 		slider.addEventListener('mouseout', () => {
+// 			rotate = setInterval(rotateSlide, speed)
+// 		});
+
+// 		function rotateSlide() {
+// 			let activeSlide = document.querySelector(`${sliderClass} > .${activeClass}`),
+// 			nextSlide = activeSlide.nextElementSibling;
+// 			if(nextSlide === null){
+// 				nextSlide = document.querySelector(`${sliderClass} > div`);
+// 			}
+
+// 			activeSlide.classList.remove(activeClass);
+// 			nextSlide.classList.add(activeClass);
 // 		}
-// 		activeSlide.removeClass(activeClass);
-// 		nextSlide.addClass(activeClass);
 // 	}
-// }
 
 // slider('introducao', 2000);
 // })();
 
 //Scroll animation
 
-(function(){ //restrigindo ao escopo
-
+(function(){
 	var $target = $('[data-anime="scroll"]'),
-	    animationClass = 'animate',
-	    offset = $(window).height() * 3/4;
+			animationClass = 'animate',
+			offset = $(window).height() * 3/4;
 
-	function animeScroll(){
-		var documentTop  = $(window).scrollTop();
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
 
 		$target.each(function(){
 			var itemTop = $(this).offset().top;
 			if (documentTop > itemTop - offset) {
-				$(this).addClass(animationClass)
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
 			}
-			else {
-				$(this).removeClass(animationClass)
-			}
-		})
+		});
 	}
 
 	animeScroll();
-	$(document).scroll(debounce(function (){
+
+	$(document).scroll(debounce(function(){
 		animeScroll();
-	}, 100));
-
+	}, 200));
 })();
-
 
 //ES6 Scroll animation
 
@@ -239,5 +231,6 @@ $('.slide > :first').addClass('active');
 //     animeScroll();
 //   }, 200));
 // }
+
 
 
